465e4d0e2300edc3bc8604b54c168d11d4eb5c59
cliffordjanuary01 <clifford.l.january@gmail.com>
2023-03-22 07:27:48 -0700

    Task 3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,19 +6,23 @@
 	 * @cmp: pointer to func of one of the 3 in main
 	 * Return: 0
 	 */
-	int int_index(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
 	{
-		int i;
-
-
-		if (array == NULL || size <= 0 || cmp == NULL)
-			return (-1);
-
-
-		for (i = 0; i < size; i++)
+		if (array != NULL && cmp != NULL)
 		{
-			if (cmp(array[i]))
-				return (i);
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
 		}
-		return (-1);
 	}
+
+	return (-1);
+}
